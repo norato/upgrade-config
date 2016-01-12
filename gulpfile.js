1b8b6f2c167188bf9b185ff0a9bf33ff5c3b9d1a
Felipe Norato <fnoratol@gmail.com>
2016-01-12 10:10:38 +0000

    Use gulp-replace-task to replace many patterns

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
-var replace = require('gulp-replace');
+var replace = require('gulp-replace-task');
 var rename = require("gulp-rename");
+
+var values = [
+  {
+    "match": /\$\{foo\}/,
+    "replacement": "works"
+  },
+  {
+    "match": /\$\{bar\}/,
+    "replacement": "works1"
+  }
+];
 
 gulp.task('upgrade-configs', function() {
   gulp.src(['Environment.example.config'])
     .pipe(rename('Environment.config'))
-
-    // Keys to replace
-    .pipe(replace(/foo/g, 'works'))
-    .pipe(replace(/bar/g, 'works1'))
-    
-
+    .pipe(replace({
+      patterns: values
+    }))
     .pipe(gulp.dest('.'));
 });
