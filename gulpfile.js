7bad9b9a040fb7e4b5551527881f8e878a515e93
Felipe Norato <fnoratol@gmail.com>
2016-01-12 10:25:23 +0000

    Works for a external json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace-task');
 var rename = require("gulp-rename");
+var hash = require('./local.json');
 
-var values = [
-  {
-    "match": /\$\{foo\}/,
-    "replacement": "works"
-  },
-  {
-    "match": /\$\{bar\}/,
-    "replacement": "works1"
-  }
-];
+var prefix = /\$\{/;
+var sufix = /\}/;
+
+var values = [];
 
 gulp.task('upgrade-configs', function() {
+  buildIt();
   gulp.src(['Environment.example.config'])
     .pipe(rename('Environment.config'))
     .pipe(replace({
@@ -21,3 +17,18 @@ gulp.task('upgrade-configs', function() {
     }))
     .pipe(gulp.dest('.'));
 });
+
+function buildIt() {
+  hash.forEach(function (element) {
+    values.push(
+      {
+        "match": buildRegex(element),
+        "replacement": element.value
+      }
+    );
+  });
+}
+
+function buildRegex (element) {
+  return new RegExp(prefix.source + element.key + sufix.source)
+}
